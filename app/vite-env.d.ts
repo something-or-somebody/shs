13506a7657fbbd41e601c67f7b2500e010daf97f
something-or-somebody <144472241+something-or-somebody@users.noreply.github.com>
2025-09-05 09:59:34 -0700

    Update vite-env.d.ts

--- app/vite-env.d.ts
+++ app/vite-env.d.ts
@@ -3,27 +3,14 @@
 declare const PRODUCTION: boolean;
 
 declare type ApplicationInfo = {
-
-    /**
-     * The name of the application.
-     */
     name: string;
-
-    /**
-     * The version of the application.
-     */
     version: string;
-
-    /**
-     * The description of the application.
-     */
     description: string;
-
-    /**
-     * The author of the application.
-     */
     author: string;
-
 }
 
 declare const APP_MANIFEST: ApplicationInfo;
+
+interface ImportMeta {
+    globEager: any;
+}
